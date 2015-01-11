2b36898f804ed6f3551be3cac249cb007cc56fd9
uugengiven <lange.john.m@gmail.com>
2015-01-10 21:39:59 -0500

    cleanup of bootstrap menu items

--- TestAngularTemplate/App_Start/BundleConfig.cs
+++ TestAngularTemplate/App_Start/BundleConfig.cs
@@ -12,11 +12,7 @@ namespace TestAngularTemplate
                 .IncludeDirectory("~/Scripts/Controllers", "*.js")
                 .Include("~/Scripts/AngularTemplate.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
